3fa89d2c130336595b05f8bbc71f3fcb553fbffb
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2022-09-12 10:27:36 -0500

    borra

--- src/checks_odoo_module.py
+++ src/checks_odoo_module.py
@@ -13,10 +13,16 @@ DFTL_README_FILES = ["README.rst", "README.md", "README.txt"]
 
 def installable(method):
     def inner(self):
-        if not self.is_module_installable:
-            print("Skipped check '%s' module '%s' is not installable" % (method.__name__, self.odoo_addon_name))
-            return True
-        return method(self)
+        msg_tmpl = "Skipped check '%s' module '%s'" % (method.__name__, self.odoo_addon_name)
+        if self.odoo_addon_name == "absa":
+            print(self.manifest_error)
+        if self.manifest_error:
+            print("%s with error: '%s'" % (msg_tmpl, self.manifest_error))
+        elif not self.is_module_installable:
+            print("%s is not installable" % (msg_tmpl))
+        else:
+            return method(self)
+
     return inner
 
 
@@ -25,6 +31,7 @@ class ChecksOdooModule:
         self.manifest_path = os.path.relpath(manifest_path)
         self.odoo_addon_path = os.path.dirname(self.manifest_path)
         self.odoo_addon_name = os.path.basename(self.odoo_addon_path)
+        self.manifest_error = ""
         self.manifest_content = self._manifest_content()
         self.is_module_installable = self._is_module_installable()
 
@@ -37,7 +44,7 @@ class ChecksOdooModule:
             try:
                 return ast.literal_eval(f_manifest.read())
             except BaseException as e:
-                print("Manifest %s with error %s" % (self.manifest_path, e))
+                self.manifest_error = "Manifest %s with error %s" % (self.manifest_path, e)
         return {}
 
     def _is_module_installable(self):
@@ -52,12 +59,20 @@ class ChecksOdooModule:
         print("Missing %s file. Template here: %s" % (DFTL_README_FILES[0], DFTL_README_TMPL_URL))
         return False
 
+    def check_manifest(self):
+        if not self.manifest_content:
+            print("%s could not be loaded" % (self.manifest_path))
+            return False
+
 
 def main_missing_readme():
     global_res = True
     for fname in sys.argv[1:]:
         obj = ChecksOdooModule(fname)
         res = obj.check_missing_readme()
+        if not res:
+            global_res = False
+        res = obj.check_manifest()
         if not res:
             global_res = False
     if not global_res:
