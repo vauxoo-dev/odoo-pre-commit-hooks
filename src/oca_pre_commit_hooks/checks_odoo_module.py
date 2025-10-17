211cfbef4c61f1e66775828ad81aa45c70a9f5e6
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2025-10-16 20:36:16 -0600

    fixup

--- src/oca_pre_commit_hooks/checks_odoo_module.py
+++ src/oca_pre_commit_hooks/checks_odoo_module.py
@@ -230,14 +230,18 @@ class ChecksOdooModule(BaseChecker):
     @utils.only_required_for_installable()
     def check_py(self):
         """Run fixit"""
-        sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))  # Enable rule path
+        # Enable rule path to be used --rules=checks_odoo_module_fixit
+        sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+        os.environ["FIXIT_ODOO_VERSION"] = self.module_version or os.getenv("VERSION") or ""
+        cmd = ["--rules=checks_odoo_module_fixit"]
+        if self.autofix:
+            cmd += ["fix", "--automatic"]
+        else:
+            cmd += ["lint"]
         # TODO: R&D to use cfg file. I tried but it was not working well
         # cfg=os.path.join(os.path.dirname(os.path.abspath(__file__)), "checks_odoo_module_fixit", "pyproject.toml")
-        # cmd = [f"--config-file={cfg}"
-        # import pdb;pdb.set_trace()
-        os.environ["FIXIT_ODOO_VERSION"] = self.module_version or os.getenv("VERSION") or ""
-        cmd = ["--rules=checks_odoo_module_fixit", "fix", "--automatic", self.odoo_addon_path]
-        fixit_main(cmd)
+        # cmd = [f"--config-file={cfg}]"
+        fixit_main(cmd + [self.odoo_addon_path])
 
 
 def lookup_manifest_paths(filenames_or_modules):
