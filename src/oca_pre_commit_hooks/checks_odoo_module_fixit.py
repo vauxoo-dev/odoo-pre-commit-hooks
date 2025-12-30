ce05e03614cba3affb708cdf25363ef15faecb32
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2025-12-30 17:25:33 -0600

    checkpoitn-hooks

--- src/oca_pre_commit_hooks/checks_odoo_module_fixit.py
+++ src/oca_pre_commit_hooks/checks_odoo_module_fixit.py
@@ -2,7 +2,6 @@
 import ast
 import glob
 import os
-import subprocess
 import sys
 from collections import defaultdict
 from functools import lru_cache
@@ -14,7 +13,7 @@ from fixit.api import fixit_paths
 from fixit.config import collect_rules, parse_rule
 from fixit.ftypes import Config, Options
 
-from oca_pre_commit_hooks import checks_odoo_module_csv, checks_odoo_module_xml, utils
+from oca_pre_commit_hooks import utils
 from oca_pre_commit_hooks.base_checker import BaseChecker
 
 colorama_init(autoreset=True)
@@ -144,21 +143,6 @@ class ChecksOdooModuleFixit(BaseChecker):
                 ext_referenced_files[ext].append(value)
         return ext_referenced_files
 
-    @utils.only_required_for_checks("manifest-syntax-error")
-    def check_manifest(self):
-        """* Check manifest-syntax-error
-        Check if the manifest file has syntax error
-        """
-        if not self.manifest_dict:
-            manifest_path_short = os.path.relpath(self.manifest_path, self.manifest_top_path)
-            self.register_error(
-                code="manifest-syntax-error",
-                message="Manifest could not be loaded",
-                info=self.error,
-                filepath=manifest_path_short,
-                line=1,
-            )
-
     @staticmethod
     def _get_module_data_files(module_root_path):
         module_root_path_obj = Path(module_root_path).resolve()
@@ -174,92 +158,6 @@ class ChecksOdooModuleFixit(BaseChecker):
                 continue
             fnames.add(subpath_obj.relative_to(module_root_path_obj).as_posix())
         return fnames
-
-    @utils.only_required_for_installable()
-    @utils.only_required_for_checks("file-not-used")
-    def check_file_not_used(self):
-        """* Check file-not-used
-        Check if there is a file created but not referenced from __manifest__.py
-        """
-        manifest_files = set()
-        for _ext, manifest_referenced_files in self.manifest_referenced_files.items():
-            for manifest_referenced_file in manifest_referenced_files:
-                filename_obj = Path(manifest_referenced_file["filename"]).resolve()
-                manifest_files.add(filename_obj.relative_to(self.odoo_addon_path).as_posix())
-        addon_files = self._get_module_data_files(self.odoo_addon_path)
-        manifest_path_short = os.path.relpath(self.manifest_path, self.manifest_top_path)
-        manual_files = self.manifest_dict.get(DATA_MANUAL_KEY) or []
-        for file_not_used in addon_files - manifest_files:
-            if file_not_used in manual_files:
-                # Ignore data files imported manually
-                # e.g. Imported from "post_init_hook" script instead of __manifest__.py "data" key
-                continue
-            file_not_used_short = os.path.join(self.odoo_addon_name, file_not_used)
-            self.register_error(
-                code="file-not-used",
-                message=f'File "{file_not_used_short}" is not referenced in the manifest.',
-                info=(
-                    f"{RED_PILL} If it is loaded from another source (e.g. a post_init_hook script),"
-                    " just add it under the section "
-                    f'"{DATA_MANUAL_KEY}": ["{file_not_used}",] to be considered. '
-                    f"{BLUE_PILL} Otherwise, you might want to remove it."
-                ),
-                filepath=manifest_path_short,
-                line=1,
-            )
-
-    @utils.only_required_for_installable()
-    @utils.only_required_for_checks("prefer-readme-rst")
-    def check_readme(self):
-        """* Check prefer-readme-rst
-        Check if the module has README.md file to prefer README.rst file
-        """
-        if self.is_message_enabled("prefer-readme-rst"):
-            readme_md = Path(self.odoo_addon_path) / "README.md"
-            if readme_md.is_file():
-                manifest_path_short = readme_md.relative_to(self.manifest_top_path)
-                self.register_error(
-                    code="prefer-readme-rst",
-                    message="Prefer README.rst instead of README.md",
-                    info=self.error,
-                    filepath=str(manifest_path_short),
-                    line=1,
-                )
-                if self.autofix:
-                    subprocess.check_output(
-                        ["git", "mv", str(manifest_path_short), str(manifest_path_short.with_suffix(".rst"))],
-                        cwd=self.manifest_top_path,
-                        stderr=subprocess.STDOUT,
-                    )
-
-    @utils.only_required_for_installable()
-    def check_xml(self):
-        manifest_datas = self.manifest_referenced_files[".xml"]
-        if not manifest_datas:
-            return
-        checks_obj = checks_odoo_module_xml.ChecksOdooModuleXML(
-            manifest_datas,
-            self.odoo_addon_name,
-            self.enable,
-            self.disable,
-            module_version=self.module_version,
-            autofix=self.autofix,
-        )
-        for check_meth in utils.getattr_checks(checks_obj):
-            check_meth()
-        self.checks_errors.extend(checks_obj.checks_errors)
-
-    @utils.only_required_for_installable()
-    def check_csv(self):
-        manifest_datas = self.manifest_referenced_files[".csv"]
-        if not manifest_datas:  # pragma: no cover
-            return
-        checks_obj = checks_odoo_module_csv.ChecksOdooModuleCSV(
-            manifest_datas, self.odoo_addon_name, self.enable, self.disable
-        )
-        for check_meth in utils.getattr_checks(checks_obj):
-            check_meth()
-        self.checks_errors.extend(checks_obj.checks_errors)
 
     @staticmethod
     @lru_cache(maxsize=32)
